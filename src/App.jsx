36024673a80172ec8c58780ad586dccfc87790ec
Tony Creavin <tonycreavin@gmail.com>
2024-06-02 19:19:29 +0100

    logout functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Home from './pages/Home';
 
 import Auth from './pages/Authentication';
 import { action as authAction } from './pages/Auth_action';
-import Signup from './pages/Signup';
+//import Signup from './pages/Signup';
+
+import { action as logoutAction } from './pages/Logout';
 //import Signin from './pages/Signin';
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
 
   { path: '/auth', element: <Auth />, action: authAction },
-  { path: '/signup', element: <Signup /> },
-  // { path: 'signin', element: <Signin /> },
+  //{ path: '/signup', element: <Signup /> },
+  { path: '/logout', action: logoutAction },
 ]);
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from '../components/Card';
 import '../App.css';
+import { CiLogin } from 'react-icons/ci';
+import getAuthToken from '../../util/auth';
+import { Form } from 'react-router-dom';
 
 function Home() {
   const [users, setUsers] = useState([]);
@@ -9,7 +12,13 @@ function Home() {
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:3000/api/v1/users');
+        const token = getAuthToken();
+        const response = await axios.get('http://127.0.0.1:3000/api/v1/users', {
+          headers: {
+            ' Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+          },
+        });
         console.log(response.data.data.users);
         setUsers(response.data.data.users);
       } catch (error) {
@@ -20,6 +29,13 @@ function Home() {
   }, []);
   return (
     <>
+      <nav className="h-[60px] w-full bg-blue-500 flex justify-end items-end p-5">
+        <Form action="/logout" method="post">
+          <button className="my-auto">
+            <CiLogin size={30} />
+          </button>
+        </Form>
+      </nav>
       <h1 className="text-black font-semibold text-center text-3xl m-2 font-marker">
         List of users
       </h1>
@@ -28,6 +44,7 @@ function Home() {
           <Card key={user._id} name={user.name} email={user.email} />
         ))}
       </div>
+      <footer className="h-[60px] w-full bg-blue-500 fixed z-1 "></footer>
     </>
   );
 }
